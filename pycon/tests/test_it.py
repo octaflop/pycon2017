3c67a1acace0f46504c78792c28dcb06f68794a2
Faris Chebib <faris@theluckybead.com>
2017-05-20 11:32:37 -0700

    added rel ref

--- pycon/tests/test_it.py
+++ pycon/tests/test_it.py
@@ -1,11 +1,14 @@
+import os
 import unittest
+
 from pycon.exception_example import get_id
 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_get_id(self):
-        fname = 'tests/test.json'
+        cwd = os.getcwd()
+        fname = os.path.join(cwd, 'pycon', 'tests', 'test.json')
         uid = get_id(conf_name=fname)
         self.assertEquals(uid, 563)
 
